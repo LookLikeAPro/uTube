1cf5c6b13e31763cf8e59f993d34e37103121d5f
jerry <jerryzhou00@gmail.com>
2014-11-07 00:23:03 -0500

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,244 @@
+angular.module('ionic.utils', [])
+
+
+var app = angular.module('uTube', ['ionic', 'ionic.utils'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+      var tag = document.createElement('script');
+      tag.src = "http://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  });
+});
+
+// Config
+
+app.config( function ($httpProvider) {
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+// Service
+
+app.service('VideosService', ['$window', '$rootScope', '$log', function ($window, $rootScope, $log) {
+    var service = this;
+    var youtube = {
+        ready: false,
+        player: null,
+        playerId: null,
+        videoId: null,
+        videoTitle: null,
+        playerHeight: '100%',
+        playerWidth: '100%',
+        state: 'stopped'
+    };
+    var results = [];
+    var upcoming = [
+        {id: 'kRJuY6ZDLPo', title: 'La Roux - In for the Kill (Twelves Remix)',
+            description: '123123 123 123'},
+        {id: '45YSGFctLws', title: 'Shout Out Louds - Illusions',
+            description: '123123 123 123'},
+        {id: 'ktoaj1IpTbw', title: 'CHVRCHES - Gun',
+            description: '123123 123 123'},
+        {id: '8Zh0tY2NfLs', title: 'N.E.R.D. ft. Nelly Furtado - Hot N\' Fun (Boys Noize Remix) HQ',
+            description: '123123 123 123'},
+        {id: 'zwJPcRtbzDk', title: 'Daft Punk - Human After All (SebastiAn Remix)',
+            description: '123123 123 123'},
+        {id: 'sEwM6ERq0gc', title: 'HAIM - Forever (Official Music Video)',
+            description: '123123 123 123'},
+        {id: 'fTK4XTvZWmk', title: 'Housse De Racket â˜â˜€â˜ Apocalypso',
+            description: '123123 123 123'}
+    ];
+    var history = [
+        {id: 'XKa7Ywiv734', title: '[OFFICIAL HD] Daft Punk - Give Life Back To Music (feat. Nile Rodgers)'}
+    ];
+    $window.onYouTubeIframeAPIReady = function () {
+        $log.info('Youtube API is ready');
+        youtube.ready = true;
+        service.bindPlayer('placeholder');
+        service.loadPlayer();
+        $rootScope.$apply();
+    };
+    function onYoutubeReady (event) {
+        $log.info('YouTube Player is ready');
+        youtube.player.cueVideoById(history[0].id);
+        youtube.videoId = history[0].id;
+        youtube.videoTitle = history[0].title;
+    }
+    function onYoutubeStateChange (event) {
+        if (event.data == YT.PlayerState.PLAYING) {
+            youtube.state = 'playing';
+        } else if (event.data == YT.PlayerState.PAUSED) {
+            youtube.state = 'paused';
+        } else if (event.data == YT.PlayerState.ENDED) {
+            youtube.state = 'ended';
+            service.launchPlayer(upcoming[0].id, upcoming[0].title);
+            service.archiveVideo(upcoming[0].id, upcoming[0].title);
+            service.deleteVideo(upcoming, upcoming[0].id);
+        }
+        $rootScope.$apply();
+    }
+    this.bindPlayer = function (elementId) {
+        $log.info('Binding to ' + elementId);
+        youtube.playerId = elementId;
+    };
+    this.createPlayer = function () {
+        $log.info('Creating a new Youtube player for DOM id ' + youtube.playerId + ' and video ' + youtube.videoId);
+        return new YT.Player(youtube.playerId, {
+            height: youtube.playerHeight,
+            width: youtube.playerWidth,
+            playerVars: {
+                rel: 0,
+                showinfo: 0
+            },
+            events: {
+                'onReady': onYoutubeReady,
+                'onStateChange': onYoutubeStateChange
+            }
+        });
+    };
+    this.loadPlayer = function () {
+        if (youtube.ready && youtube.playerId) {
+            if (youtube.player) {
+                youtube.player.destroy();
+            }
+            youtube.player = service.createPlayer();
+        }
+    };
+    this.launchPlayer = function (id, title) {
+        youtube.player.loadVideoById(id);
+        youtube.videoId = id;
+        youtube.videoTitle = title;
+        return youtube;
+    }
+    this.listResults = function (data) {
+        results.length = 0;
+        for (var i = data.items.length - 1; i >= 0; i--) {
+            results.push({
+                id: data.items[i].id.videoId,
+                title: data.items[i].snippet.title,
+                description: data.items[i].snippet.description,
+                thumbnail: data.items[i].snippet.thumbnails.default.url,
+                author: data.items[i].snippet.channelTitle
+            });
+        }
+        return results;
+    }
+    this.queueVideo = function (id, title) {
+        upcoming.push({
+            id: id,
+            title: title
+        });
+        return upcoming;
+    };
+    this.archiveVideo = function (id, title) {
+        history.unshift({
+            id: id,
+            title: title
+        });
+        return history;
+    };
+    this.deleteVideo = function (list, id) {
+        for (var i = list.length - 1; i >= 0; i--) {
+            if (list[i].id === id) {
+                list.splice(i, 1);
+                break;
+            }
+        }
+    };
+    this.getYoutube = function () {
+        return youtube;
+    };
+    this.getResults = function () {
+        return results;
+    };
+    this.getUpcoming = function () {
+        return upcoming;
+    };
+    this.getHistory = function () {
+        return history;
+    };
+}]);
+app.factory('$localstorage', ['$window', function($window) {
+    return {
+        set: function(key, value) {
+            $window.localStorage[key] = value;
+        },
+        get: function(key, defaultValue) {
+            return $window.localStorage[key] || defaultValue;
+        },
+        setObject: function(key, value) {
+            $window.localStorage[key] = JSON.stringify(value);
+        },
+        getObject: function(key, defaultValue) {
+            return JSON.parse($window.localStorage[key] || JSON.stringify(defaultValue));
+        }
+    }
+}]);
+// Controller
+app.controller('VideosController', function ($scope, $http, $log,$ionicSideMenuDelegate, VideosService, $localstorage) {
+
+    $scope.youtube = VideosService.getYoutube();
+    $scope.results = VideosService.getResults();
+    $scope.upcoming = VideosService.getUpcoming();
+    $scope.history = VideosService.getHistory();
+    $scope.playlist = true;
+    $scope.test= $localstorage.get('count',67);
+    $scope.tab = [true,false,false];
+
+    $scope.launch = function (id, title) {
+        VideosService.launchPlayer(id, title);
+        VideosService.archiveVideo(id, title);
+        //VideosService.deleteVideo($scope.upcoming, id);
+        $log.info('Launched id:' + id + ' and title:' + title);
+    };
+
+    $scope.queue = function (id, title) {
+        VideosService.queueVideo(id, title);
+        VideosService.deleteVideo($scope.history, id);
+        $log.info('Queued id:' + id + ' and title:' + title);
+    };
+
+    $scope.delete = function (list, id) {
+        VideosService.deleteVideo(list, id);
+    };
+
+    $scope.search = function () {
+        $http.get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+                key: 'AIzaSyA57XvCbZFEjA30XKLlEYBSVJGprGswIU4',
+                type: 'video',
+                maxResults: '8',
+                part: 'id,snippet',
+                fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle',
+                q: this.query
+            }
+        })
+            .success( function (data) {
+                VideosService.listResults(data);
+                $log.info(data);
+            })
+            .error( function () {
+                $log.info('Search error');
+            });
+    };
+
+    $scope.tabulate = function (state) {
+        $scope.playlist = state;
+    };
+    $scope.tab = function (id){
+        for (i=0;i<3;i++) {
+            $scope.tab[i] = false;
+        }
+        $scope.tab[id]=true;
+    };
+
+});
